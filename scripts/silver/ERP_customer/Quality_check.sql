d5a7c46c938de2e0a758030719488f30187e6166
rdhuwaraha21 <pdhuwaraha@gmail.com>
2025-03-14 18:21:42 +0530

    Update Quality_check.sql

--- scripts/silver/ERP_customer/Quality_check.sql
+++ scripts/silver/ERP_customer/Quality_check.sql
@@ -1,1 +1,19 @@
+/* cid  */
+SELECT
+    cid,
+    bdate,
+    gen
+FROM bronze.erp_cust_az12
+WHERE cid LIKE '%AW00011000%'
 
+/* bdate  */
+-- Identify Out-of-Range Dates
+SELECT DISTINCT
+    bdate
+FROM bronze.erp_cust_az12
+WHERE bdate < '1924-01-01' OR bdate > GETDATE()
+
+/* gen  */
+-- Data Standardization & Consistency
+SELECT DISTINCT gen
+FROM bronze.erp_cust_az12
