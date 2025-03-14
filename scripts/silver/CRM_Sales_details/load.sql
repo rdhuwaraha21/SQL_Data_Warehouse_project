39e9d6679769161245a3ea7ff957264befcb64c6
rdhuwaraha21 <pdhuwaraha@gmail.com>
2025-03-14 17:46:42 +0530

    Update load.sql

--- scripts/silver/CRM_Sales_details/load.sql
+++ scripts/silver/CRM_Sales_details/load.sql
@@ -1,1 +1,40 @@
+insert into silver.crm_sales_details(
+			sls_ord_num,
+			sls_prd_key,
+			sls_cust_id,
+			sls_order_dt,
+			sls_ship_dt,
+			sls_due_dt,
+			sls_sales,
+			sls_quantity,
+			sls_price
+)
 
+SELECT
+	sls_ord_num,
+	sls_prd_key,
+	sls_cust_id,
+	CASE 
+		WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+	END AS sls_order_dt,
+	CASE 
+		WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+	END AS sls_ship_dt,
+	CASE 
+		WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
+		ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+	END AS sls_due_dt,
+	CASE 
+		WHEN sls_sales is null or sls_sales <=0 or sls_sales != sls_quantity * abs(sls_price)
+				THEN sls_quantity * abs(sls_price)
+		ELSE sls_sales
+	END AS sls_sales,
+	sls_quantity,
+	CASE 
+		WHEN sls_price is null or sls_price <=0 
+				THEN sls_sales / nullif (sls_quantity,0)
+		ELSE sls_sales
+	END AS sls_price
+FROM bronze.crm_sales_details
