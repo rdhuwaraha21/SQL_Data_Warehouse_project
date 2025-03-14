110341a6f1b468186705da9d4131cc7519f8cdb9
rdhuwaraha21 <pdhuwaraha@gmail.com>
2025-03-14 18:23:03 +0530

    Update transformation.sql

--- scripts/silver/ERP_customer/transformation.sql
+++ scripts/silver/ERP_customer/transformation.sql
@@ -1,1 +1,25 @@
+/*cid*/
+SELECT
+			CASE
+					WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4,LEN (cid))
+					ELSE cid
+			END AS cid,
+FROM bronze.erp_cust_az12
 
+/*bdate*/
+-- Identify Out-of-Range Dates
+SELECT DISTINCT
+ CASE WHEN bdate > GETDATE() THEN NULL
+      ELSE bdate
+ END AS bdate
+FROM bronze.erp_cust_az12
+
+/*gen*/
+-- Data Standardization & Consistency
+SELECT DISTINCT
+    CASE 
+        WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
+        WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
+        ELSE 'n/a'
+    END AS gen
+FROM bronze.erp_cust_az12
