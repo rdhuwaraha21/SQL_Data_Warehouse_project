1401a71f8135ad9d2af19d224a1fb2e4b955c636
rdhuwaraha21 <pdhuwaraha@gmail.com>
2025-03-14 19:03:05 +0530

    Update Quality_check.sql

--- scripts/silver/ERP_product_category/Quality_check.sql
+++ scripts/silver/ERP_product_category/Quality_check.sql
@@ -1,1 +1,18 @@
+/*Check for unwanted Spaces */
+
+SELECT * FROM bronze.erp_px_cat_g1v2
+WHERE cat != TRIM(cat) 
+   OR subcat != TRIM(subcat) 
+   OR maintenance != TRIM(maintenance);
+
+/*Data Standardization & Consistency*/
+
+SELECT DISTINCT cat
+FROM bronze.erp_px_cat_g1v2;
+
+SELECT DISTINCT subcat
+FROM bronze.erp_px_cat_g1v2;
+
+SELECT DISTINCT maintenance
+FROM bronze.erp_px_cat_g1v2;
 
