dcd68dd84ddcea1c522d35183c47a31665a29b80
rdhuwaraha21 <pdhuwaraha@gmail.com>
2025-03-14 17:43:16 +0530

    Update Quality_check.sql

--- scripts/silver/CRM_Sales_details/Quality_check.sql
+++ scripts/silver/CRM_Sales_details/Quality_check.sql
@@ -1,1 +1,70 @@
+/* sls_ord_num */
+--space check
+SELECT 
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+sls_order_dt,
+sls_ship_dt,
+sls_due_dt,
+sls_sales,
+sls_quantity,
+sls_price
+FROM bronze.crm_sales_details
+WHERE sls_ord_num != TRIM(sls_ord_num)
+
+/* prd_key */
+-- any prd_key not in crm_prd_info
+SELECT
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+sls_order_dt,
+sls_ship_dt,
+sls_due_dt,
+sls_sales,
+sls_quantity,
+sls_price
+FROM bronze.crm_sales_details
+WHERE sls_prd_key NOT IN (SELECT prd_key FROM silver.crm_prd_info)
+
+/* cst_id */
+-- any cst_id not in crm_cst_info
+SELECT
+sls_ord_num,
+sls_prd_key,
+sls_cust_id,
+sls_order_dt,
+sls_ship_dt,
+sls_due_dt,
+sls_sales,
+sls_quantity,
+sls_price
+FROM bronze.crm_sales_details
+WHERE sls_cust_id NOT IN (SELECT cst_id FROM silver.crm_cust_info)
+
+/* Date fields - order_date, ship_date, due_date */
+-- Check for Invalid Dates (apply for all dates)
+SELECT
+NULLIF(sls_order_dt, 0) sls_order_dt
+FROM bronze.crm_sales_details
+WHERE sls_order_dt <= 0
+OR LEN(sls_order_dt) != 8
+OR sls_order_dt > 20500101
+OR sls_order_dt < 19000101
+
+/* Data consistency and sales = Qty * price */
+--Data consistency: between sales qty, price
+--Sales = Qty * price
+-- no negative, zero, null
+
+select distinct
+sls_sales,
+sls_quantity,
+sls_price
+from bronze.crm_sales_details
+where sls_sales != sls_quantity * sls_price
+or sls_sales is null or sls_quantity is null or sls price is null
+or sls_sales <= 0 or sls_quantity <= 0 or sls price <= 0
+order by sls_sales,sls_quantity,sls_price
 
