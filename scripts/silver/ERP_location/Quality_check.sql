228eee2dcc59c5be55f87ee80aac02011e37e22f
rdhuwaraha21 <pdhuwaraha@gmail.com>
2025-03-14 18:40:15 +0530

    Update Quality_check.sql

--- scripts/silver/ERP_location/Quality_check.sql
+++ scripts/silver/ERP_location/Quality_check.sql
@@ -1,1 +1,14 @@
+/*cid*/
+SELECT 
+    cid, 
+    cntry 
+FROM bronze.erp_loc_a101;
+
+SELECT cst_key FROM silver.crm_cust_info;
+
+/*cntry*/
+-- Data Standardization & Consistency
+SELECT DISTINCT cntry
+FROM bronze.erp_loc_a101
+ORDER BY cntry;
 
