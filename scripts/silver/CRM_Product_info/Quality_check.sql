e7aace73e57c26a575ae7b4395ddc31d93acbfdf
rdhuwaraha21 <pdhuwaraha@gmail.com>
2025-03-14 16:06:36 +0530

    Update Quality_check.sql

--- scripts/silver/CRM_Product_info/Quality_check.sql
+++ scripts/silver/CRM_Product_info/Quality_check.sql
@@ -1,1 +1,27 @@
+/* Begining - bronze , after transformation - change to silver */
 
+/* Null & Duplicate */
+-- prd_id -- primary key
+SELECT prd_id, COUNT(*) 
+FROM bronze.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) > 1 OR prd_id IS NULL
+
+-- prd_cost
+select prd_cost 
+from bronze.crm_prd_info
+where prd_cost < 0 or prd_cost is null
+
+/* Unwanted spaces */
+SELECT prd_nm
+FROM bronze.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm)
+
+/* Data standardization & consistency */
+select distinct prd_line
+from bronze.crm_prd_info
+
+/* Date related (start_date & end_date) */
+select * 
+from bronze.crm_prd_info
+where prd_end_dt < prd_start_dt
